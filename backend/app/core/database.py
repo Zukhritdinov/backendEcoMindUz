0b3b057cf59c574648dbb41eb268d3afc41ff75e
Zuhritdinov Abduvahob <abduvakhob1999@gmail.com>
2026-04-22 13:37:00 +0300

    Data base ulandi

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Uses local database for dev outside docker or docker db container depending on env
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost:5432/ecominduz"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:dUUGWvFJgxWmhVIamicsgQSvgiduUFQc@postgres.railway.internal:5432/railway"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
